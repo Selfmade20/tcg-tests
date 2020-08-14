cc3bbf9d5131e175938bee8645139721a21ec87d
Selfmade20 <tebogo1686@gmail.com>
2020-08-14 10:47:45 +0200

    finalizing the running port

--- productivity-app/backend/server.js
+++ productivity-app/backend/server.js
@@ -18,3 +18,11 @@ const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully!")
 })
+
+const usersRouter = require('./routes/users');
+
+app.use('/users', usersRouter)
+
+app.listen(port, () => {
+    console.log(`Server is running on port:${5000}`);
+})
