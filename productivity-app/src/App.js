c948a7b291a8a0af62808fd4938132d27c62f480
Selfmade20 <tebogo1686@gmail.com>
2020-08-14 11:53:35 +0200

    added the todo route

--- productivity-app/src/App.js
+++ productivity-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { NavigationBar } from './components/NavigationBar';
 import Register from './components/Register';
-import './App.css'
+import Todos from './Todo';
 
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
       <Router>
     <Switch>
       <Route path='/register' component={Register} />
-      <Route path="*" component={() => "404 NOT FOUND"} />
-
+      <Route exact path='/todos' component={Todos} />
+      {/* <Route path="*" component={() => "404 NOT FOUND"} /> */}
     </Switch>
       </Router>
   </React.Fragment>
