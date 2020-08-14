d8ded2595fc3889964e90b54d9baefbb752264c0
Selfmade20 <tebogo1686@gmail.com>
2020-08-14 11:24:49 +0200

    added the post-todo route

--- productivity-app/backend/routes/users.js
+++ productivity-app/backend/routes/users.js
@@ -1,15 +1,41 @@
 const router = require('express').Router();
-let User = require('../models/user.model');
+const User = require('../models/user.model');
+const Todo = require('../models/todo.model')
 
-router.route('/register').post(async () => {
+
+router.route('/register').post(async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
 
-    const newUser = newUser({
+    const newUser = new User({
         email,
-        password
+        password,
     })
+
+    try{
+        await newUser.save()
+        return res.send(newUser).json("User Registered!")
+    }catch (err) {
+        return res.status(400).json("Error:" + err)
+    }
+})
+
+router.route('/addTodo').post(async (req, res) => {
+    const description = req.body.description;
+    const status = req.body.status
+
+    const newTodo = new Todo({
+        description,
+        status
+    })
+
+    try{
+        await newTodo.save()
+        return res.send(newTodo).json("User Registered!")
+    }catch (err) {
+        return res.status(400).json("Error:" + err)
+    }
 })
 
 
